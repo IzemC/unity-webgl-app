5f073f1f6c1f013fa72ab9ca7696b8c10ba5236f
Tahar <tahar@jungle.co>
2025-05-16 08:08:53 +0400

    rotation button component

---
+++ src/components/hud/rotation-control-button.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { cn } from "@/utils/cn";
+
+interface RotationControlButtonProps
+  extends React.HTMLAttributes<HTMLButtonElement> {
+  label: string;
+  dirKey: string;
+  rotate: (key: string, pressed: boolean) => void;
+}
+
+export const RotationControlButton: React.FC<RotationControlButtonProps> = ({
+  label,
+  className,
+  dirKey,
+  rotate,
+  ...props
+}) => {
+  return (
+    <button
+      onPointerDown={() => rotate(dirKey, true)}
+      onPointerUp={() => rotate(dirKey, false)}
+      onPointerLeave={() => rotate(dirKey, false)}
+      className={cn(
+        "size-8 md:size-16 focus-visible:outline-none rounded-full bg-gray-800/80 flex items-center justify-center pointer-events-auto",
+        className
+      )}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
